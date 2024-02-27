76ef67f86d888613c27e7554dc71d28cbd9a2b57
Skye Purchase <atp45@cam.ac.uk>
2024-02-27 13:22:16 +0000

    Add basic end points

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import ChemRoute from './routes/ChemistryChecker';
+import NuclearRoute from './routes/NuclearChecker';
+import ChemParserRoute from './routes/ChemistryParser';
+import NuclearParserRoute from './routes/NuclearParser';
 
 dotenv.config();
 
@@ -8,6 +12,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use('/chem', ChemRoute);
+app.use('/nuclear', NuclearRoute);
+app.use('/chem-parser', ChemParserRoute);
+app.use('/nuclear-parser', NuclearParserRoute);
 
 app.get('/', (_req: Request, res: Response) => {
       res.send('The Nu-Chem Checker');
---
+++ src/routes/NuclearParser.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from "express";
+import { ValidationChain, validationResult } from "express-validator";
+
+const router = Router();
+
+const nuclearValidationRules: ValidationChain[] = [];
+
+router.post('/', nuclearValidationRules, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const str: string = JSON.stringify(req.body, null, 4);
+    console.log(`[server]: /nuclear-parser recieved ${str}`);
+    res.status(501).send("Not Implemented\n");
+});
+
+export default router;
