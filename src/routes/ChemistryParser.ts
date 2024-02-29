74fdcac1950e03bc6f2f35a611de229041d39a76
Skye Purchase <atp45@cam.ac.uk>
2024-02-29 13:18:29 +0000

    Implement parser endpoints for both checkers

--- src/routes/ChemistryParser.ts
+++ src/routes/ChemistryParser.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
-import { ValidationChain, validationResult } from "express-validator";
+import { ValidationChain, body, validationResult } from "express-validator";
+import { parseChemistryExpression } from "inequality-grammar";
 
 const router = Router();
 
-const chemValidationRules: ValidationChain[] = [];
+const chemValidationRules: ValidationChain[] = [
+    body('test').notEmpty().withMessage("mhChem expression is required."),
+    body('description').optional().isString().withMessage("When provided the descrition must be a string.")
+];
 
 router.post('/', chemValidationRules, (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -12,9 +16,13 @@ router.post('/', chemValidationRules, (req: Request, res: Response) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const str: string = JSON.stringify(req.body, null, 4);
-    console.log(`[server]: /chem-parser recieved ${str}`);
-    res.status(501).send("Not Implemented\n");
+    const parse: ChemAST = parseChemistryExpression(req.body.test)[0];
+    res.status(201).send(parse);
+
+    const str: string = JSON.stringify(parse, null, 4);
+    const request: string = req.body.description ? " '" + req.body.description + "'" : "";
+    console.log(`[server]: Parsed request${request}`);
+    console.log(`[server]: \n${str}`);
 });
 
 export default router;
--- src/routes/NuclearParser.ts
+++ src/routes/NuclearParser.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
-import { ValidationChain, validationResult } from "express-validator";
+import { ValidationChain, body, validationResult } from "express-validator";
+import { parseNuclearExpression } from "inequality-grammar";
 
 const router = Router();
 
-const nuclearValidationRules: ValidationChain[] = [];
+const nuclearValidationRules: ValidationChain[] = [
+    body('test').notEmpty().withMessage("mhChem expression is required."),
+    body('description').optional().isString().withMessage("When provided the descrition must be a string.")
+];
 
 router.post('/', nuclearValidationRules, (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -12,9 +16,13 @@ router.post('/', nuclearValidationRules, (req: Request, res: Response) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const str: string = JSON.stringify(req.body, null, 4);
-    console.log(`[server]: /nuclear-parser recieved ${str}`);
-    res.status(501).send("Not Implemented\n");
+    const parse: NuclearAST = parseNuclearExpression(req.body.test)[0];
+    res.status(201).send(parse);
+
+    const str: string = JSON.stringify(parse, null, 4);
+    const request: string = req.body.description ? " '" + req.body.description + "'" : "";
+    console.log(`[server]: Parsed request${request}`);
+    console.log(`[server]: \n${str}`);
 });
 
 export default router;
